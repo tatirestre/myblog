e0a38bf3276d990244409e69869a4ac269edcbfb
Tatiana Restrepo <trestrepo@lat-trestrepo.local>
2015-11-09 10:53:26 -0500

    created comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+	validates_presence_of :name, :content
+	validates_uniqueness_of :name
+	has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109024254) do
+ActiveRecord::Schema.define(version: 20151109153446) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user"
+    t.text     "user_comment"
+    t.integer  "post_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
