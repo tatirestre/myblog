23a4649e74e7dc81cc429f34f5abc1ae707bdc02
Tatiana Restrepo <trestrepo@lat-trestrepo.local>
2015-11-16 18:05:59 -0500

    created association bet users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,5 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	has_many :post_categories
 	has_many :categories, through: :post_categories
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116221923) do
+ActiveRecord::Schema.define(version: 20151116224319) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151116221923) do
     t.string   "featured_image"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
