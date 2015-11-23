f814b6972802f80d9295b0b1a00fa88d05b41eb3
Tatiana Restrepo <trestrepo@lat-trestrepo.local>
2015-11-23 16:49:50 -0500

    Post is now asking for user email as a dropdown

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :featured_image, :user, { category_ids: []})
+      params.require(:post).permit(:name, :content, :featured_image, :user_id, { category_ids: []})
     end
 end
